556edcd06487b6815eddd7b6adb96cd8f439c2a5
Jong Hoon Kim <kimiyo@naver.com>
2019-05-04 09:52:55 +0900

    Update in the client to make a conflict

--- vs-practices/python-ex/demo.py
+++ vs-practices/python-ex/demo.py
@@ -3,9 +3,10 @@ def hello(p1,p2):
     print('Hi ',p1)
     print('Hi there',p2)
 
-hello('good!','good')
-a=1 #update line
+hello('good!','good') #Update in client
 hello('a','b')
-hello("oh",'Boy')
+hello("New","Line")
+hello("oh",'Hello') #Small update
 #this is a new line
-
+#Hey, this is another udpate in master branch.
+#Wonderful feature in GIT.
