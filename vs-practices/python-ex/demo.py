904fedb7fd0a2225361b54279775cea3f6f95a2c
JHKim <kimiyo@naver.com>
2019-05-04 09:52:24 +0900

    Update demo.py
    Updated in the web

--- vs-practices/python-ex/demo.py
+++ vs-practices/python-ex/demo.py
@@ -3,7 +3,7 @@ def hello(p1,p2):
     print('Hi ',p1)
     print('Hi there',p2)
 
-hello('good!','good')
+hello('good!','good') #I like update in the web directly
 a=1 #update line
 hello('a','b')
 hello("oh",'Boy')
